ca8ca49f7e4340d6ceb6ac8c202c2da95ac60bf2
bruno <b_linhares@outlook.com>
2024-08-15 12:37:58 -0300

    feat inclusao de paginas parciasi e estaticas

--- djangoapp/produto/views/ListaProdutos.py
+++ djangoapp/produto/views/ListaProdutos.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views import View
 from django.http import HttpResponse
+from produto import models # type: ignore
 
 
 # Create your views here.
 class ListaProdutos(ListView):
-    def get(self, *args, **kwargs):
-        return HttpResponse(f'{self.__class__.__name__}')
+    model = models.Produto
+    template_name = 'produto/lista.html'
